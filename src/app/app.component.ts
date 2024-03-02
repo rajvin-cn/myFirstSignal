891fb7fe9ba204134ab2b0c5ca58ae615fa3d849
rajvin-cn <rajvin.k2020@gmail.com>
2024-03-02 19:10:04 -0330

    first commit setinterval example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 
+  import {Component, computed, effect, EffectRef, signal} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFirstSignal';
+
+  signalQty = signal<number>(1);
+  
+  computedValue = computed( () => this.signalQty()+5);
+
+
+
+constructor() {
+
+  
+  setInterval( () =>{
+    console.log("calling update value for "+ this.signalQty());
+    this.signalQty.update( (qty) => qty +1)}, 100);
+
+    effect (() => console.log('curr val is '+ this.signalQty()));
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
